50704deb5436b48e9d9c9f88621d6b66d013c4bb
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-02-29 23:04:30 +0530

    Update 1.hello.py

--- 1 Hello_World/1.hello.py
+++ 1 Hello_World/1.hello.py
@@ -4,5 +4,16 @@ print("Hello World")
 # Anything starting with # is Single Line Comment
 print(5 / 8)
 
-# Print the sum of 7 and 10
+# Simple Calculations
 print(7 + 10)
+print(3 * 5)
+print(10 / 2)
+# Modulo - Prints Remainder of Division
+print(18 % 7)
+
+# Some More Calculation Below
+# Exponentiation : 4 raise to 2 ( 4 ^ 2 )
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years? (If you get 10% Interest Every Year)
+print(100*(1.1**7))
