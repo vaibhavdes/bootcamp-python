715db650d5a7774c931e2288618ed027bdac3508
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-03-15 23:25:30 +0530

    Update list.py
    List Introduction

--- 2 Lists/list.py
+++ 2 Lists/list.py
@@ -1,1 +1,24 @@
+  # In Previous Lesson we learned different data types as follows
+  # int = integer
+  # float = floating
+  # str = string
+  # bool = boolean
+  
+  # In this we gonna learn about making Data Points (Which are inter-related to each other) this can be used in future for analysis.
+  
+  # List (Values are wrapped in Square Brackets [...] )
+  # 1. Collection of Values
+  # 2. Contain values of type (As Per Mentioned Data Types Above and advanced types)
+  # 3. Contain values of Different Types (Can Contain Lists in List i.e Sub-List)
+  
+  # Lets take an example below list contain family member name with age
+  my_family = ['x',21,'y',24]
+  type(my_family)
+  
+  # Also it can be represented as Sub-List
+  my_family = [['x',21],
+               ['y',24]
+              ]
+  type(my_family)
+  
   
