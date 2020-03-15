c71e3e16fa6bcd348064664926f9fb934bfdcc2f
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-03-16 00:40:54 +0530

    Update list.py

--- 2 Lists/list.py
+++ 2 Lists/list.py
@@ -54,6 +54,35 @@
   # [start:] = element till end
   print(areas[5:])
 
+  print(areas[1] + areas[3])
 
+  # create a new variable, eat_sleep_area, that contains the sum of the area of the kitchen and the area of the bedroom.
+  eat_sleep_area = areas[3] + areas[-3]
     
+  # create a list, downstairs, that contains the first 6 elements of areas.
+  downstairs = areas[0:6]
+  downstairs = areas[:6]
+
+  # Do a similar thing to create a new variable, upstairs, that contains the last 4 elements of areas.
+  upstairs = areas[6:10]
+  upstairs = areas[6:]
   
+  # Try ( In Python Shell )
+  x = [["a", "b", "c"],
+     ["d", "e", "f"],
+     ["g", "h", "i"]]
+  
+  x[2]
+  
+  x[2][0]
+  
+  x[2][:2]
+
+  # Try
+  house = [['hallway', 11.25],
+           ['kitchen', 18.0],
+           ['living room', 20.0],
+           ['bedroom', 10.75],
+           ['bathroom', 9.5]]
+  
+  house[-1][1]
