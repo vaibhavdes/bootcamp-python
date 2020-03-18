3e9ff2470e80dbf24a61830c00994c8ee723d8c0
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-03-18 22:03:22 +0530

    Update function.py
    Revision

--- 3 Functions/function.py
+++ 3 Functions/function.py
@@ -1,1 +1,21 @@
 
+  # Function is a piece of code for particular functionality or to get  processed output when certain input is provided.
+  # We already used few of python function, do you remember ? e.g print(), type()
+  # Function are reusable
+  
+  # Try this
+  var1 = [1, 2, 3, 4]
+  var2 = True
+  
+  # Use print() in combination with type() to print out the type of var1.
+  print(type(var1))
+  
+  # Use len() to get the length of the list var1. Wrap it in a print() call to directly print it out.
+  print(len(var1))
+
+  # Use int() to convert var2 to an integer. Store the output as out2.
+  out2 = int(var2)
+
+  # To Know About Any Function and What argument does it take 
+  help(type)
+  # Also there are thousand of function in python, if you need for anything search on internet
