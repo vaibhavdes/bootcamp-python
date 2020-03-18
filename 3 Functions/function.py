c767df1b4fdf54a991bf3ad7d924874dc592d35e
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-03-18 23:05:05 +0530

    Update function.py

--- 3 Functions/function.py
+++ 3 Functions/function.py
@@ -16,9 +16,21 @@
   # Use int() to convert var2 to an integer. Store the output as out2.
   out2 = int(var2)
 
-  # To Know About Any Function and What argument does it take 
+  # To Know About Any Function and What argument does it take (Few arguments might be optional)
   help(type)
   help(max)
   ?max
   ?type
   # Also there are thousand of function in python, if you need for anything search on internet
+
+  # Lets learn one new function i.e sorted()
+  # Ref https://docs.python.org/2/glossary.html#term-iterable
+  # Try
+  first = [11.25, 18.0, 20.0]
+  second = [10.75, 9.50]
+  
+  # Use + to merge the contents of first and second into a new list: full.
+  full = sorted(first)  + sorted(second)
+  # Call sorted() on full and specify the reverse argument to be True. Save the sorted list as full_sorted.
+  full_sorted = sorted(full,reverse=True)
+  print(full_sorted)
