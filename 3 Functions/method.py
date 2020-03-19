5735d31c65f627506bd9b69fc5445b80ab1847cf
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-03-19 22:36:16 +0530

    Update method.py

--- 3 Functions/method.py
+++ 3 Functions/method.py
@@ -62,4 +62,13 @@
   first.append(21.30)
   print(first)
   
+  # Lets try few Method for String Operation
+  place = "poolhouse"
+
+  # Use the upper() method on place and store the result in place_up. Use the syntax for calling methods that you learned in the previous video.
+  place_up = place.upper()
+  print(place)
+  print(place_up)
   
+  # Print out the number of o's on the variable place by calling count() on place and passing the letter 'o' as an input to the method. We're talking about the variable place, not the word "place"!
+  print(place.count('o'))
