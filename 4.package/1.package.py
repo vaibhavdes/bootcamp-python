7a864688f0d2cd3a8014c433a841fa6ac139d26d
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2020-03-19 23:54:28 +0530

    Create 1.package.py
    Packages

---
+++ 4.package/1.package.py
@@ -0,0 +1,14 @@
+# Lets understand this new topic 
+# Package is a folder/directory
+#          -> (which contain different .py files called scripts or module)
+#            -> (Each script/module file can contain many resuable specific functions, methods, and types )
+
+# There are thousands of package distributed for various use like :
+# numpy (mathematical operations)
+# matplotlib (for graph visualization of data)
+# scikit-learn (machine learning)
+
+# To use this package, we need to install it via package installer using terminal
+# Read https://pip.readthedocs.io/en/stable/installing/
+
+# To use in your .py script it needs to be imported 
